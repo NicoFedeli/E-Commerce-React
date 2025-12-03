c86127a36f5f8d671e0ac0856aa66c3d233c2f71
Nicoló Fedeli <nicolo.fedeli.priv@gmail.com>
2025-12-03 14:19:01 +0100

    cart fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,7 +34,7 @@ function App() {
             <Link to="/">E-Commerce</Link>
           </div>
           <div className="header-links">
-            <a href="cart.html">Cart</a>
+            <a href="cart">Cart</a>
             {userInfo ? (
               <Link to="/profile">{userInfo.name}</Link>
             ) : (
